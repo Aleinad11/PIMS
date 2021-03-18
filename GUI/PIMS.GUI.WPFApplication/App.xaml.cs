d30670263d38fda5b07b0ec0ac968d79527caf70
Michael Hamilton <mghamilton.panda@gmail.com>
2021-03-17 20:38:35 -0500

    Add project files.

---
+++ GUI/PIMS.GUI.WPFApplication/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PIMS.GUI.WPFApplication.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace PIMS.GUI.WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
