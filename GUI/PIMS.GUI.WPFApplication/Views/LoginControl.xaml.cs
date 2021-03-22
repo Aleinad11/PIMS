bc2f7b693e26a3c9eab1df591f81800643f2eebf
Michael Hamilton <mghamilton.panda@gmail.com>
2021-03-22 17:20:51 -0500

    References and LoginControl/ViewModel added

---
+++ GUI/PIMS.GUI.WPFApplication/Views/LoginControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PIMS.GUI.WPFApplication.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginControl
+    /// </summary>
+    public partial class LoginControl : UserControl
+    {
+        public LoginControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
